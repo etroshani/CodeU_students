ff28fa29b257231da588ca858744c5814311a1b1
etroshani <114190660+etroshani@users.noreply.github.com>
2022-09-24 18:29:54 -0400

    Add files via upload

---
+++ Class1_studentversion.py
@@ -0,0 +1,265 @@
+#Welcome to your 1st class with CODEU!
+
+#   In the 1st class, we are covering these topics:
+#       -print()
+#       -Defining variables
+#       -Mathematical equations and operations: Using Python as a calculator
+#       -Strings, Integers, Floats and Booleans
+#       -Input
+
+
+####    FIRST PART    ####
+
+
+##   1. Your first code!    ##
+
+#   Code 'Hello World' and other possible Strings using print().
+
+#   Defining a variable
+"""
+INSERT CODE HERE
+
+"""
+
+#   Overriding a variable
+"""
+INSERT CODE HERE
+
+"""
+
+#   Combining Strings
+"""
+INSERT CODE HERE
+
+"""
+
+
+##   2. Turning Python into a Calculator!   ##
+
+#   Resolve mathematical formulas using int and float.
+#   Additions
+
+# Example 1
+"""
+INSERT CODE HERE
+
+"""
+
+# Example 2
+"""
+INSERT CODE HERE
+
+"""
+
+# Example 3
+"""
+INSERT CODE HERE
+
+"""
+
+# More efficient way to add to variable x
+"""
+INSERT CODE HERE
+
+"""
+
+# Example 4
+"""
+INSERT CODE HERE
+
+"""
+
+
+#   Substractions
+
+# Example 5
+"""
+INSERT CODE HERE
+
+"""
+
+# Example 6
+"""
+INSERT CODE HERE
+
+"""
+
+# Example 7
+"""
+INSERT CODE HERE
+
+"""
+
+
+#   Multiplications
+
+# Example 8
+"""
+INSERT CODE HERE
+
+"""
+
+# Example 9
+"""
+INSERT CODE HERE
+
+"""
+
+#   Modulo
+"""
+INSERT CODE HERE
+
+"""
+
+#   Floor Division
+"""
+INSERT CODE HERE
+
+"""
+
+#   Rounding
+"""
+INSERT CODE HERE
+
+"""
+
+
+##  3. Introduction to Operators, Arithmetic Operations, and Booleans  ## 
+# This will be very useful in something called CONDITIONALS and LOOPS. We will cover this in future projects and exercises!
+
+# '==' --> is equal to
+"""
+INSERT CODE HERE
+
+"""
+
+# '<' --> less than
+"""
+INSERT CODE HERE
+
+"""
+
+# '<=' --> less than or is equal to
+"""
+INSERT CODE HERE
+
+"""
+
+# '>' --> greater than
+"""
+INSERT CODE HERE
+
+"""
+
+# '>=' --> greater than or is equal to
+"""
+INSERT CODE HERE
+
+"""
+
+# '!=' --> is NOT equal to
+"""
+INSERT CODE HERE
+
+"""
+
+##   Introduction to AND, OR and NOT statements  ##
+
+# And
+"""
+INSERT CODE HERE
+
+"""
+
+# OR
+"""
+INSERT CODE HERE
+
+"""
+
+# NOT
+"""
+INSERT CODE HERE
+
+"""
+
+
+
+####    SECOND PART    ####
+
+#   In the second part, we'll learn how to use INPUT. This will be the first interactive code you will make!
+#   You'll learn how to use INPUT with strings, integers and floats. At the end, you'll code a small exercise where
+#   you have to create a list of guests!
+
+
+##  4. Strings, Integers, Floats and Booleans  ##
+
+# String
+strings = "A string is a sequence of characters that is read as ONE element. It can be letters, numbers, symbols, and emojis"
+
+# Integer
+integers = 5    # An Integer(int) is a value used to represent real numbers that DO NOT HAVE decimals("numbers after the comma").
+
+# Float
+floats = 5.55   # A Float(float) is a value used to represent real numbers that HAVE decimals("numbers after the comma").
+
+#Boolean
+Booleans = True    # Boolean statements are statements that evaluate to be true or false.
+Booleans = False   # This is often used in functions and conditionals.
+
+
+####  5. Introduction to INPUT!  ####
+
+# Example 1: Insert your name. This will be a String
+"""
+INSERT CODE HERE
+
+"""
+
+# Example 2: Multiplying two numbers. This will be a two Strings that we will convert to Integers to make the calculations
+"""
+INSERT CODE HERE
+
+"""
+
+
+
+###     HOMEWORK   |    Creating an interactive calculator! ###
+
+# Create a calculator that can do additions, substractions and multiplications. Make sure to greet the user.
+#       1) Greet the user using INPUT and PRINT()
+#       2) Addition: Ask for two numbers using INPUT and print the result of the addition
+#       3) Substraction: Ask for two more numbers and print the result of the substraction
+#       4) Multiplication: Ask for three more numbers and print the result of the multiplication
+#       5) Thank them for using the your Calculator
+
+
+# Greet the user
+"""
+INSERT CODE HERE
+
+"""
+
+# Addition of 2 numbers entered by the user
+"""
+INSERT CODE HERE
+
+"""
+
+# Subtraction of 2 numbers entered by the user
+"""
+INSERT CODE HERE
+
+"""
+
+# Multiplication of 3 numbers entered by the user
+"""
+INSERT CODE HERE
+
+"""
+
+# Thank you message
+"""
+INSERT CODE HERE
+
+"""
