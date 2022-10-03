9ab2f863e7dfb76660d0c98fb06f6f78b5cb1b9d
etroshani <114190660+etroshani@users.noreply.github.com>
2022-10-03 18:05:30 -0400

    Add files via upload

---
+++ Class2_studentversion.py
@@ -0,0 +1,163 @@
+#Welcome to your 2nd CODEU class!
+
+#   In the 2nd class, we are covering these topics:
+#       -Input()
+#       -Float
+#       -Conditionals
+
+
+
+####    FIRST PART     ####
+
+##   Concepts that we learned last class:   ## 
+#       -print()
+#       -Defining variables
+#       -Mathematical equations and operations: Using Python as a calculator
+#       -Strings, Integers, Floats and Booleans
+#       -Input
+
+
+
+####    SECOND PART    ####
+
+##     1. Continuation of INPUT     ##
+
+# Example: What year are you born in?
+"""
+INSERT CODE HERE
+
+"""
+
+#   Small Exercise: What is the exact weight of an ant colony?   #
+#   **Float is important here**
+""" 
+******DONT ERASE THE FOLLOWING PRINT LINES******
+print("\nYou face a colony of ants. The average ant weighs about 0.225 pounds. There is about 300 ants in front of you.")
+print("You have to calculate the EXACT total weight of the colony to be able to pass.\n")
+
+INSERT CODE HERE
+
+"""
+
+#   Is it possible to combine a float with an Int?
+"""
+INSERT CODE HERE
+
+"""
+
+
+##   Big Exercise: Creating a list of VIP guests using name, age and height  ##
+#   You are the host of the annual CodeU birthday bash! You are given the responsibility of creating a small VIP guest list
+#   Unfortunately, there can only be 3 VIP guests this year. You must take down each guests NAME, AGE, and HEIGHT(cm) using
+#   INPUT() and PRINT(). Each guest needs to be displayed with all his\her attributes using PRINT().
+
+#   VIP #1   #
+"""
+INSERT CODE HERE
+
+"""
+
+#   VIP #2   #
+"""
+INSERT CODE HERE
+
+"""
+
+#   VIP #3   #
+"""
+INSERT CODE HERE
+
+"""
+
+#   Print all VIP members with their attributes   #
+"""
+******DONT ERASE THE FOLLOWING PRINT LINES******
+print("\nVIP #1")
+
+INSERT CODE HERE
+
+"""
+
+"""
+print("\nVIP #2")
+
+INSERT CODE HERE
+
+"""
+
+"""
+print("\nVIP #3")
+
+INSERT CODE HERE
+
+
+
+print("\nVIP list is complete! Have a wonderful CodeU Birthday BASH!!")
+"""
+
+##     2) Introduction to CONDITIONALS     ##
+
+# Example 1
+"""
+INSERT CODE HERE
+
+"""
+
+
+# Example 2
+"""
+INSERT CODE HERE
+
+"""
+
+
+# Small exercise using INPUT: Are you old enough to ride the rollercoaster?
+# Introduction to ELIF
+"""
+INSERT CODE HERE
+
+"""
+
+###        HOMEWORK        ###
+#   For your birthday party, you have a VIP section. Only 2 people are allowed to TRY to enter. To enter, 
+#   they must guess the correct number between 1 and 5 that YOU choose using INPUT().You must take down their 
+#   First Names, Last Names, and their guess. 
+
+#   1) Welcome your guests!
+#   2) Define a variable with a number between 1 and 5. Use Input()
+#   3) You have a VIP section and only 2 people are allowed to TRY to enter.
+#   4) You have to take down their FIRST NAMES and their GUESS. 
+#   5) If they guess incorrectly, they CAN'T be part of the VIP list. You must let them know they can't be a VIP member.
+#   6) If they guess correctly, you must greet them using their name and welcome them to the VIP section.
+#   7) Print() a message saying the VIP entrance is closed once all tries are done.
+
+
+###     HOMEWORK SOLUTION     ###
+
+# Define your number between 1-5 and welcome everyone to your party!
+"""
+INSERT CODE HERE
+
+"""
+
+# First Guest
+"""
+INSERT CODE HERE
+
+"""
+
+# Second Guest
+"""
+INSERT CODE HERE
+
+"""
+
+# Vip is closed message
+"""
+INSERT CODE HERE
+
+"""
+
+
+
+
