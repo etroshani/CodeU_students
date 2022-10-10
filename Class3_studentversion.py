90129f2ec1fbab9eb4f959ff8f806ac3e0f333ff
etroshani <114190660+etroshani@users.noreply.github.com>
2022-10-10 01:18:52 -0400

    Add files via upload

---
+++ Class3_studentversion.py
@@ -0,0 +1,162 @@
+#Welcome to your 3rd CODEU class!
+
+#   In the 3rd class, we are covering these topics:
+#       Conditionals: IF, ELIF, ELSE
+#       Time Module
+
+
+####    FIRST PART     ####
+
+##   1) Concepts that we learned last class:   ## 
+#       -Input()
+#       -Float
+#       -Conditionals: IF, ELIF, ELSE
+
+##   2) Homework Revision   ##
+
+
+
+####    SECOND PART    ####
+
+##     1) Exercise - Science Query: How well do you know your planets?      ##
+""" *Don't remove the following print()*
+print("Welcome to a Science Query on Planets! You must answer the following questions correctly.")
+print("\nLet's see how well you know your solar system...")
+
+INSERT CODE HERE
+
+"""
+
+
+
+####        2) Exercise of the day: JUMANJI        ####
+# You have to create your own JUMANJI Adventure. You create a character with a NAME, AGE and WEIGHT using INPUT.
+# That character then has to face challenges in the jungle where they are always given 2 choices. Ultimately, one of the
+# choices leads to losing the game and the game ends. The other choice leads to the next challenges.
+# REQUIREMENTS:
+#       -There has to be 3 challenges MINIMUM
+#       -There are 2 choices per challenge
+#       -Make it creative and FUN! 
+
+
+""" *Don't remove the following print()*
+
+
+import time
+
+print("Welcome to Jumanji!")
+time.sleep(2)
+print("\nLong ago, the island of JUMANJI was a happy and prosperous place for life.")
+time.sleep(3)
+print("\nAnimals and plants lived in harmony, and the entire island was a heavenly location.")
+time.sleep(3)
+print("\nUntil one day...")
+time.sleep(2)
+print("\nA group of money-hungry adventurers decided to hunt down the powerful and valuable ruby...")
+time.sleep(3)
+print("\nAfter weeks of restless search, the found it...and took it!")
+time.sleep(3)
+print("\nOn their way back to their car, one of the adventurers got greedy...")
+time.sleep(3)
+print("\nHe attacked his coworkers and stole the ruby for himself!")
+time.sleep(3)
+print("\nAs he was escaping by the mountains, he tripped and dropped the ruby down the cliff, into a deep dark cave")
+time.sleep(3)
+print("\nAnd the ruby was never seen again...")
+time.sleep(3)
+print("\nThe island of JUMANJI has, since then, started to lose its life little by little, day by day.")
+time.sleep(3)
+print("\nThe island will soon die unless YOU can find the ruby and save JUMANJI!")
+time.sleep(3)
+print("\nYou must enter the island and complete the challenges ahead without dying to fetch back the lost ruby of JUMANJI")
+time.sleep(2)
+"""
+
+# First Question: Will you play?
+"""
+print("\nDo you wish to enter JUMANJI in hopes of saving the island? ")
+
+INSERT CODE HERE
+
+"""
+
+    # Second Question: Pit of Snakes!
+    """
+    print("\nYou are faced with a pit of snakes. You can either fight them or jump over them. ")
+
+    INSERT CODE HERE
+
+    """
+        # Third Question: Which road will you choose?
+        """
+        print("\nAfter a long walk, the road you are on splits in two. You must decide wether to go left or right.")
+        time.sleep(3)
+        print("\nThe path on the right is dark and scary. The one on the left is sunny and bright. Choose wisely...")
+        
+    
+        INSERT CODE HERE
+
+        """
+
+            # Fourth question: Pick your guide
+            """
+            print("\nYou must choose between a hungry lion and a monkey to guide you through.")
+
+            
+            INSERT CODE HERE
+
+            """
+        
+
+
+
+
+
+####    HOMEWORK: Create your own ICE CREAM STORE!    ####
+
+#   You will build an ice-cream store where clients will walk in and be welcomed!
+#   1) You must give your store a name
+#   2) Customers will then have the choice of Chocolate and Vanilla ice cream.
+#   3) They can choose between 3 sizes: Small(s), Medium(m) and Large(l)
+#   4) You must let them know their total once they have ordered!
+#   Here are the prices:
+#   | Vanilla |
+#       Small : 2$
+#       Medium : 5$
+#       Large : 10$ However, it's currently OUT OF STOCK
+#   | Chocolate |
+#       Small : 3$
+#       Medium : 10$ However, they ordered the 100th medium chocolate ice-cream today so they get it for free!
+#       Large : 30$
+
+
+####    HOMEWORK SOLUTION    ####
+
+# Define your store name using INPUT() and welcome your guests to the store. Include your store name in the welcome message.
+"""
+INSERT CODE HERE
+
+"""
+
+# Define the flavor ice cream the customer wants using INPUT()
+"""
+INSERT CODE HERE
+
+"""
+
+# Define the size the customer wants using INPUT()
+"""
+INSERT CODE HERE
+
+"""
+
+# Write your conditionals. A small portion of the code is already written to help you out.
+"""
+if icecream == "vanilla":
+    if size == "small":
+        print("\nYour total will be 2$")
+
+INSERT CODE HERE
+
+"""
+
