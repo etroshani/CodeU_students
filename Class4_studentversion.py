dd901b5f4f5666d15c8f4cff64fede7237a61de9
etroshani <114190660+etroshani@users.noreply.github.com>
2022-10-15 15:04:06 -0400

    Add files via upload

---
+++ Class4_studentversion.py
@@ -0,0 +1,168 @@
+#Welcome to your 4th CODEU class!
+
+
+#   In the 4th class, we are covering these topics:
+#       Lists
+#       Loops: WHILE
+
+
+
+####    FIRST PART    ####
+
+##  1) Recap of what was taught last class:   ## 
+#       Conditionals: If, Elif, Else
+#       Time Module
+
+
+##  2) Homework Revision   ##
+
+
+
+####    SECOND PART    ####
+
+##     1) Introduction to Lists     ##
+
+# Creating and Printing out a list.
+"""
+INSERT CODE HERE
+
+"""
+
+# Lists can contain different types of elements.
+"""
+INSERT CODE HERE
+
+"""
+
+# You can retrieve a specific element of a List using the INDEX number.
+"""
+INSERT CODE HERE
+
+"""
+
+# A section of the list can be grabbed
+"""
+INSERT CODE HERE
+
+"""
+
+# Modifying an element in a list
+"""
+INSERT CODE HERE
+
+"""
+
+# Adding an element to the end of the list
+"""
+INSERT CODE HERE
+
+"""
+
+# A list can be empty. To add elements, we use append.
+"""
+INSERT CODE HERE
+
+"""
+
+# Adding an element at a specific position in the list
+"""
+INSERT CODE HERE
+
+"""
+
+# Erasing an element in the list
+"""
+INSERT CODE HERE
+
+"""
+
+# Detecting wether an element is in the list or not using Boolean
+"""
+INSERT CODE HERE
+
+"""
+
+# Finding the length of the list
+"""
+INSERT CODE HERE
+
+"""
+
+
+
+##     2) Introduction to Loops: WHILE     ##
+
+
+# Example 1 
+"""
+INSERT CODE HERE
+
+"""
+
+# Example 2
+"""
+INSERT CODE HERE
+
+"""
+
+# Example 3 - Can we go to the movies?
+"""
+INSERT CODE HERE
+
+"""
+
+
+##     2) Tiny Exercise - Let's build a Countdown for the CodeU Track Race!      ##
+
+"""
+print("Welcome to the annual CodeU Track Race!\n")      #Do not remove this line
+print("The race will now start in...")                  #Do not remove this line
+
+
+INSERT CODE HERE
+
+
+
+print("Go Go Go!")                                      #Do not remove this line
+"""
+
+####    HOMEWORK: Create your Wish List Maker!    ####
+
+#   You will build a Wish List Maker for anyone that would like to make a list of presents they would love to receive in the future!
+#   1) You must create an empty list where each gift will be added
+#   2) You will then define a variable using INPUT(). This is where the user is asked what present they would like to add to the list.
+#   3) The list can contain only 5 items.
+#   4) Create a WHILE Loop that ends once the list LENGTH is equal to 5. 
+#   5) Print() the completed list to the user
+
+
+
+####    HOMEWORK CORRECTION    ####
+
+# 1st Part : Create an empty list
+"""
+INSERT CODE HERE
+
+"""
+
+
+# 2nd Part : Create the WHILE Loop that ends once the list LENGTH is 5
+
+# Explanation: The user will be asked to add a present to the list 5 separate times. This means that the same question is
+#              repeated 5 times. Each answer is added to the list.
+"""
+INSERT CODE HERE
+
+"""
+
+# 3rd Part : Print() the list
+"""
+print("Here is your Wish List!")    #Do not remove this line
+
+INSERT CODE HERE
+
+"""
+
+
+
+    
