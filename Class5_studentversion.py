3d01d8fcdebe6ef3cf5d0303bba4f0848e82586e
etroshani <114190660+etroshani@users.noreply.github.com>
2022-10-25 18:58:18 -0400

    Add files via upload

---
+++ Class5_studentversion.py
@@ -0,0 +1,168 @@
+#Welcome to your 5th CODEU class!
+
+
+#   In the 5th class, we are covering these topics:
+#       Loops: WHILE
+#       Loops: FOR
+
+
+
+####    FIRST PART    ####
+
+##  1) Recap of what was taught last class:   ## 
+#       Lists
+#       Loops: WHILE
+
+
+##  2) Homework Revision   ##
+
+
+
+####    SECOND PART    ####
+
+
+##     1) First Exercise Of The Day - Guess The Number!      ##
+
+# Introduction to the RANDOM Module #
+"""
+INSERT CODE HERE
+
+"""
+
+
+##    2) Metaverse Guessing Contest!    ##
+"""
+
+
+
+print("Welcome to Radio Metaverse Guessing Game! Today, we have a contest for you!") 
+time.sleep(2)
+print("\nYou must guess a number between 1 and 10. You have 3 tries to guess it correctly.") 
+time.sleep(2)
+print("\nIf you do...You win entrance to the METAVERSE!")
+time.sleep(2) 
+print("\nCall us at the Metaverse Radio Station to enter the contest.")
+time.sleep(2)
+
+
+INSERT CODE HERE
+
+"""
+
+
+##     3) Introduction to Loops: FOR     ##
+
+# Explain the functionality of FOR Loops. How they work and when to use them.
+
+
+# We want to print all elements of a list. This method works, however it is not ideal if our list contains many elements.
+"""
+INSERT CODE HERE
+
+"""
+
+# Instead, we use FOR loop to retrieve the elements in a list
+"""
+INSERT CODE HERE
+
+"""
+
+# Find the SUM of all the elements in a list
+"""
+INSERT CODE HERE
+
+"""
+
+# Finding each element of a String (word, sequence of letters, etc.)
+"""
+INSERT CODE HERE
+
+"""
+
+# Introduction to RANGE()
+"""
+INSERT CODE HERE
+
+"""
+
+# range(START, STOP, STEP). The first value is the start of the range, the second is the end of the range, and the third is the increment of the step.
+"""
+INSERT CODE HERE
+
+"""
+
+
+##      Final Exercise of the day: Hidden passage in the hills!     ##
+"""
+print("During a hike, you come across a hidden entrance in the mountains...")
+time.sleep(2)
+print("\nYou try to enter, but the entrance seems locked...")
+time.sleep(2)
+print("\nNext to the passage, you see a sentence engraved in the wall")
+time.sleep(2)
+print("\nAnd it says the following...")
+time.sleep(2)
+print("\nTo enter, you must find the sum of all numbers between 1 and 99")
+time.sleep(2)
+
+***
+INSERT CODE HERE
+***
+
+## DO NOT ERASE THE FOLLOWING LINES ##
+answer = input("Please enter your answer: ")
+if int(answer) == 4950:
+    time.sleep(2)
+    print("\nYou may enter the hidden passage in the hills!")
+else:
+    time.sleep(2)
+    print("\nThat answer is incorrect!")
+
+"""
+
+
+####    HOMEWORK: Will the elevator break?    ####
+
+#   A famous hotel in New York City has a faulty and very weak elevator. They've held back on fixing it
+#   because they are lazy and don't want to spend money. 
+#   By precaution, the can only allow 8 people on the elevator at a time.
+#   A group of tourists arrive at the hotel. They all book a room for the night. Since they're all together,
+#   they decide to go up at the same time. We have the weight of each person in pounds:
+#   208, 200, 350, 500, 127, 145, 134, 900
+#   The elevator can only hold a MAXIMUM of 2500 pounds. You must determine if the elevator will break
+#   if they all use it at the same time!
+
+#   1) You must create a list with all weights
+#   2) You will then define a variable for the total weight.
+#   3) You must use a FOR Loop to navigate the list you made
+#   4) Add each individual weight to the variable for total weight
+#   5) Print() the total weight once the loop is complete
+#   6) Print() the number of people on the elevator using a list method
+
+
+
+####    HOMEWORK CORRECTION    ####
+
+# Build the list of tourists
+"""
+INSERT CODE HERE
+
+"""
+
+# Create the variable where you will be adding the weight of every person
+"""
+INSERT CODE HERE
+
+"""
+
+# Create the FOR Loop
+"""
+INSERT CODE HERE
+
+"""
+
+# Print the results
+"""
+INSERT CODE HERE
+
+"""
