88561d6164bb13b22a88c22e0f48ff581574acd5
etroshani <114190660+etroshani@users.noreply.github.com>
2022-11-06 17:44:23 -0500

    Add files via upload

---
+++ Class7_studentversion.py
@@ -0,0 +1,108 @@
+#Welcome to your 7th CODEU class!
+
+
+#   In the 7th class, we are covering these topics:
+#       Project: CodeU Dodgeball Game!
+
+
+
+####    FIRST PART    ####
+
+##  1) Recap of what was taught last class:   ## 
+#       Loops: FOR
+#       Loop controls: CONTINUE and BREAK statements
+
+
+##  2) Homework Revision   ##
+
+
+
+####    SECOND PART    ####
+
+##     CodeU Dodeball Game!     ##
+"""
+import random
+import time
+
+print("\nWelcome to a game of CodeU Dodgeball!")
+time.sleep(2)
+print("\nYou are faced with the infamous CodeU Robot at a game of Dodgeball...")
+time.sleep(3)
+print("\nThe Robot, being a master of dodgeball, unfortunately has an advantage where it can generate a ball at anytime without having to pick it up.")
+time.sleep(4)
+print("\nLuckily for you, the Robot has an issue...")
+time.sleep(2)
+print("\nIt sometimes has a completely RANDOM malfunction where it cannot move...")
+time.sleep(2)
+print("\nOtherwise, it can either shoot a ball or dodge. Any action from the Code Robot is done randomly.")
+time.sleep(3)
+print("\nThis makes it completely unpredictable...")
+time.sleep(2)
+print("\nYou, on the other hand, can either shoot a dodgeball, pick-up a dodgeball, or dodge an attack.")
+time.sleep(3)
+print("\nUnfortunately, you are unable to shoot if you haven't picked up at least one dodgeball!")
+time.sleep(3)
+print("\nConsidering the robot is a master at dodgeball, it'll only have 3 lives, whereas you will have 5.")
+time.sleep(3)
+print("\nYou start the game with NO balls in your possession!")
+time.sleep(3)
+print("\nYou must do your best to defeat the CodeU Robot and become the champion...")
+time.sleep(3)
+print("\nGood luck!")
+"""
+
+# Set your Attributes
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Build your code
+"""
+
+INSERT CODE HERE
+
+"""
+
+    # Printing the updated Attributes
+    """
+
+    INSERT CODE HERE
+
+    """
+
+    # Your actions
+    """
+
+    INSERT CODE HERE
+
+    """
+    
+    # CodeU Robot's actions: 1 -> Throws a ball, 2 -> Malfunctions, 3 -> Dodges
+    """
+
+    INSERT CODE HERE
+
+    """
+
+    # Code ALL the different scenarios
+    """
+
+    INSERT CODE HERE
+
+    """
+
+# Final print of the attributes
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Results once a Loop condition is filled
+"""
+
+INSERT CODE HERE
+
+"""
