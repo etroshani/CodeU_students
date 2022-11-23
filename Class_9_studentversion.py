ede5a64cab20f470a922df4f9d8f3320d02e401c
etroshani <114190660+etroshani@users.noreply.github.com>
2022-11-23 01:54:54 -0500

    Add files via upload

---
+++ Class_9_studentversion.py
@@ -0,0 +1,271 @@
+#Welcome to your 9th CODEU class!
+
+
+#   In this class, we are covering these topics:
+#       - Introduction to the Turtle Module
+#       - Turtle Exercisees
+
+
+
+####    FIRST PART    ####
+
+##     Introduction to the TURTLE Module     ##
+
+# We must always start our TURTLE programs with import turtle 
+# and finish the code with turtle.done() so that the window remains open
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Initiate a Turtle pen
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+##  Properties of the Turtle pen  ##
+# Color
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Pen Size
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Pen Shape
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Pen Speed: This allows use to determine the speed that the pen will draw!
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+##  Pen Manipulations  ##
+
+# Pen Movements
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Pen Rotations
+"""
+
+INSERT CODE HERE
+
+"""
+
+#Go To and Set Position functions: This allows us to move the pen around to specific coordinates!
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Pen up and Pen down functions: This allows us to move the pen on our canvas without drawing!
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Multiple pens on the same canvas
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+
+####    SECOND PART    ####
+
+##   Exercise 1: Draw a square!   ##
+"""
+
+INSERT CODE HERE
+
+"""
+
+##   Exercise 2: Draw a circle!   ##
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Fill and End Fill: This allows us to fill the shapes we draw with color!
+# The first color("green") is the color of the pen. The second("yellow") is the color of the fill!
+"""
+
+INSERT CODE HERE
+
+"""
+
+###    Final Exercise Of The Day: Build Traffic Lights!    ###
+
+## 1) Create Pens ##
+"""
+
+INSERT CODE HERE
+
+"""
+
+## 2) Start Drawing ##
+#--Red Light--#
+"""
+
+INSERT CODE HERE
+
+"""
+
+#--Yellow Light--#
+# Set the position for the yellow pen
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Draw
+"""
+
+INSERT CODE HERE
+
+"""
+
+#--Green Light--#
+# Set the position for the green pen
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Draw
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+
+####    HOMEWORK: Olympic Rings!    ####
+
+#   The commissioner of the Olympics has asked you to create the Olympic rings using Python!
+#   The Olympic rings contain 5 rings of different colors: Blue, Black, Red, Green, and Yellow
+#   1) Each ring has a radius of 50
+#   2) The distance between each ring is 80
+
+#   STEPS:
+#       1) Create 2 pens for the first and second row of rings
+#       2) Set the position for the first ring on the first row
+#       3) Draw all 3 rings of the first wor
+#       4) Set the position for the first ring on the second row
+#       5) Draw the last rings
+
+
+####    HOMEWORK CORRECTION    ####
+
+# Create 2 separate pens for the 2 rows or rings
+"""
+
+INSERT CODE HERE
+
+"""
+
+## First Row Of Rings##
+# 1) First Ring: Blue
+# Set the position for the first ring
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Draw the ring
+"""
+
+INSERT CODE HERE
+
+"""
+
+# 2) Second Ring: Black
+# Set the position for the second ring
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Draw the ring
+"""
+
+INSERT CODE HERE
+
+"""
+
+# 3) Third Ring: Red
+# Set the position for the third ring
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Draw the ring
+"""
+
+INSERT CODE HERE
+
+"""
+
+## Second Row Of Rings ##
+# 1) Fourth Ring: Green
+# Set the position for the first ring
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Draw the ring
+"""
+
+INSERT CODE HERE
+
+"""
+
+# 2) Fifth Ring
+# Set the position for the fifth ring
+"""
+
+INSERT CODE HERE
+
+"""
+
+# We can now draw our final circle!!!
+"""
+
+INSERT CODE HERE
+
+"""
