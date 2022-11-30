886dbddb0d4e4c72c780ab735123fc51b2ff9347
etroshani <114190660+etroshani@users.noreply.github.com>
2022-11-29 20:54:58 -0500

    Add files via upload

---
+++ Class10_studentversion.py
@@ -0,0 +1,227 @@
+#Welcome to your 10th CODEU class!
+
+#   In this class, we are covering these topics:
+#       - Creating shapes efficiently
+#       - Complex Turtle Exercises
+
+
+####    FIRST PART    ####
+
+##   Recap of what was taught last class:   ## 
+#       - Introduction to the Turtle Module
+#       - Turtle Exercises
+
+
+##   REVIEW HOMEWORK   ##
+# Review the homework to correct any mistake they may have done or to answer any question they may have.
+
+
+
+####    SECOND PART    ####
+
+##     Drawing shapes efficiently using Loops!    ##
+import turtle
+
+# Create your pen
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Triangle
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Square
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Hexagon
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Star
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+##     Exercise 1: Choose your shape!     ##
+"""
+print("You can choose to draw a variety of shapes with different colors!") 
+print("\nHere are your choices of shapes: circle | triangle | square | rectangle | rectangle | star")
+print("\nHere are your choices of colors: blue | green | yellow | red | pink | purple | brown | beige")
+print("\nYou can choose a color for the outline of your shape, and a different color for the inside!")
+"""
+
+# Set your conditionals and input function inside a Loop
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+
+##     Final Exercise Of The Day: Random circles on the screen!     ##
+import random
+
+listColors = ["green", "yellow", "blue", "red", "cyan", "orange", "pink", "black", "purple"]
+
+"""
+
+INSERT CODE HERE
+
+"""
+    # Set your colors
+    """
+
+    INSERT CODE HERE
+
+    """
+    
+    # Set your positions
+    """
+
+    INSERT CODE HERE
+
+    """
+
+    # Draw your circle
+    """
+
+    INSERT CODE HERE
+
+    """
+
+
+
+####    HOMEWORK: Build your house!    ####
+
+#   The mayor of your town has asked you to draw a sketch of a house to determine what types of houses he should build in the city!
+#   The house can be of any shape or form, be any color, and be any size. However, there are requirements.
+#   Requirements:
+#   1) The house must have a ROOF(triangular, etc.)
+#   2) The house must have a DOOR(rectangle, circle, etc.)
+#   3) The house must have AT LEAST 1 WINDOW(circle, square, etc.)
+
+#   STEPS:
+#       1) Create your pen
+#       2) Set a an outline color and a fill color for each element of your drawing (Ex.: Door)
+#       3) Set a starting position when drawing an element
+#       4) Draw the element in the MOST efficient way
+
+
+####    HOMEWORK CORRECTION    ####
+
+##  Create your pen  ##
+"""
+
+INSERT CODE HERE
+
+"""
+
+##  House  ##
+# Color
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Position
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Draw House
+"""
+
+INSERT CODE HERE
+
+"""
+
+##  ROOF  ##
+# Color
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Position
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Draw Roof
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+##  WINDOW  ##
+# Color
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Position
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Draw Window
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+##  DOOR  ##
+# Color
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Position
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Draw Door
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+turtle.done()
