1af4cf5bda236d99d47486b1901186f4e5e805a4
etroshani <114190660+etroshani@users.noreply.github.com>
2022-12-06 17:37:25 -0500

    Add files via upload

---
+++ Class_11&12_studentversion.py
@@ -0,0 +1,123 @@
+#Welcome to your 11th and 12th CODEU class!
+
+
+#   In these classes, we are covering:
+#       - Final CodeU Project: TURTLE RACE
+
+
+
+####    FIRST PART    ####
+
+##   Recap of what was taught last class:   ## 
+#       - Efficient ways to draw shapes
+#       - Complex Turtle Exercises
+
+
+##   HOMEWORK REVISION   ##
+
+
+
+####    SECOND PART    ####
+
+
+####        Final CodeU Project: TURTLE RACE        ####
+
+# Import the necessary modules for this project
+"""
+
+INSERT CODE HERE
+
+"""
+
+## SCREEN BACKGROUND ##
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+## FINISH LINE ##
+
+"""
+
+INSERT CODE HERE
+
+"""
+
+# First vertical line of squares
+"""
+
+INSERT CODE HERE
+
+"""
+
+# Second vertical line of squares
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+## OUR TURTLES ##
+
+# 1st Turtle
+"""
+
+INSERT CODE HERE
+
+"""
+
+# 2nd Turtle
+"""
+
+INSERT CODE HERE
+
+"""
+
+# 3rd Turtle 
+"""
+
+INSERT CODE HERE
+
+"""
+
+# 4th Turtle 
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+# Set a slight delay before the race starts
+"""
+
+INSERT CODE HERE
+
+"""
+
+
+## TURTLE MOVEMENT ##
+
+# Create a while loop that will keep looping until a turtle has reached a specific position, the finish line.
+"""
+
+INSERT CODE HERE
+
+"""   
+
+    # The turtle are able to move forward by 1 to 5 pixels determined randomly
+    """
+
+    INSERT CODE HERE
+
+    """
+    
+    # Set the conditionals where if a turtle reaches the finish line(x>=240) or further, the turtle wins and the loop is broken.
+    """
+
+    INSERT CODE HERE
+
+    """
